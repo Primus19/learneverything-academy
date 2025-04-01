3fadfaf5b25148da537d4d29ba11dace74d43e0b
Primus Vekuh <primus.vekuh@gmail.com>
2025-04-01 14:30:25 -0500

    added button files

---
+++ app/components/ui/card-direct/CardDescription.jsx
@@ -0,0 +1,3 @@
+export default function CardDescription({ children }) {
+  return <p className="text-gray-500 text-sm">{children}</p>;
+}
---
+++ app/components/ui/card-direct/CardTitle.jsx
@@ -0,0 +1,3 @@
+export default function CardTitle({ children }) {
+  return <h3 className="text-lg font-bold">{children}</h3>;
+}
